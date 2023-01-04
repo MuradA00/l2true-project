2aa866fcbe30c7a22ff586198602f8b31efeded7
Murad <abishlimurad@gmail.com>
2023-01-04 15:57:18 +0200

    layout: modal, scripts: burger, modal, mobile menu links

--- source/js/common.js
+++ source/js/common.js
@@ -1,7 +1,24 @@
 const header = document.querySelector('.header'),
       main = document.querySelector('main'),
       sidebarToggleButton = document.querySelectorAll('.sidebar__icon'),
-      sidebarHiddenInfo = document.querySelectorAll('.sidebar-column__info');
+      sidebarHiddenInfo = document.querySelectorAll('.sidebar-column__info'),
+      descriptionBody = document.querySelector('.description'),
+      sidebar = document.querySelector('.sidebar'),
+      body = document.body,
+      burger = document.querySelector('.header__burger'),
+      menu = document.querySelector('.menu'),
+      closeIcon = document.querySelector('.menu__close'),
+      modalTriggers = document.querySelectorAll('.downloadBtn'),
+      modalDownload = document.querySelector('.modal'),
+      modalClose = document.querySelector('.modal__close'),
+      mobNavLinks = document.querySelectorAll('.menu-nav__link');
+
+modalTriggers.forEach(trigger => {
+  trigger.addEventListener('click', () => {
+    modalDownload.classList.add('show-modal')
+    body.style.overflow = 'hidden';
+  })
+})
 
 sidebarToggleButton.forEach(btn => {
   btn.addEventListener('click', (e) => {
@@ -9,9 +26,49 @@ sidebarToggleButton.forEach(btn => {
   })
 })
 
+function closeMenuByClick() {
+  mobNavLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('show-menu')
+      body.classList.remove('body-locked');
+      burger.classList.remove('active-burger')
+    })
+  })
+}
+
+function closeModal() {
+  modalDownload.classList.remove('show-modal')
+  body.style.overflow = '';
+}
+
+function closeMenu() {
+  closeIcon.classList.add('closed');
+  menu.classList.remove('show-menu');
+  burger.classList.remove('active-burger');
+  body.classList.remove('body-locked');
+}
+
+function showMenu() {
+  closeIcon.classList.remove  ('closed')
+  burger.classList.toggle('active-burger');
+  body.classList.add('body-locked')
+  if (burger.classList.contains('active-burger')) {
+    menu.classList.add('show-menu')
+    body.classList.add('body-locked')
+  } else {
+    menu.classList.remove('show-menu')
+    body.classList.remove('body-locked');
+  }
+}
+
 function setSpacing() {
   const headersHeight = header.clientHeight;
   main.style.marginTop = `${headersHeight}px`;
 }
 
+modalClose.addEventListener('click', closeModal);
 setSpacing();
+closeMenuByClick();
+burger.addEventListener('click', showMenu);
+closeIcon.addEventListener('click', closeMenu);
+
