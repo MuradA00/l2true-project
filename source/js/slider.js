1cb136aed09d08b17079b8c966874ec579a3dfb8
Murad <abishlimurad@gmail.com>
2023-01-05 01:36:33 +0200

    fixes: descr page adaptive issues, layout: slider section building

---
+++ source/js/slider.js
@@ -0,0 +1,11 @@
+const mainSlider = new Swiper('.beta__wrapper', {
+  sliderPerView: 1,
+  allowTouchMove: true,
+  navigation: {
+    nextEl: '.beta-control__btn--next',
+    prevEl: '.beta-control__btn--prev'
+  },
+  pagination: {
+    el: '.beta-control__bullets'
+  }
+});
