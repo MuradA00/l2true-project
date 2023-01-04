46fc6b7473b41e1505065f0a668859a7c17b542c
Murad <abishlimurad@gmail.com>
2023-01-04 04:39:07 +0200

    layout: description page

--- source/js/common.js
+++ source/js/common.js
@@ -1,5 +1,13 @@
 const header = document.querySelector('.header'),
-      main = document.querySelector('main');
+      main = document.querySelector('main'),
+      sidebarToggleButton = document.querySelectorAll('.sidebar__icon'),
+      sidebarHiddenInfo = document.querySelectorAll('.sidebar-column__info');
+
+sidebarToggleButton.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    btn.nextElementSibling.classList.toggle('show-hidden-info');
+  })
+})
 
 function setSpacing() {
   const headersHeight = header.clientHeight;
